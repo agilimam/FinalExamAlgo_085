341e40a2122a68bb2767788cdf10d7d3c073e4b9
agilimam <agilimamalhafizi04@gmail.com>
2023-07-17 13:20:10 +0700

    Add project files.

---
+++ uasalgoritma/uasalgoritma.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa{
+public:
+	int NIM;
+	string jurusan;
+	string nama;
+	int tahun;
+};
+
+
+const int MAX_MAHASISWA = 100;
+int NIM[MAX_MAHASISWA];
+string nama[MAX_MAHASISWA];
+int tahunMasuk[MAX_MAHASISWA];
+int jumlahMahasiswa = 0;
+
+
+void tambahMahasiswa(
+	if (jumlahMahasiswa < MAX_MAHASISWA) {
+		cout << "========== TAMBAH MAHASISWA ==========" << endl;
+		cout << "NIM : ";
+		cin >> NIM[jumlahMahasiswa];
+		cin.ignore();
+		cout << "Nama : ";
+		getline(cin, nama[jumlahMahasiswa]);
+		cout << "Jurusan : ";
+		getline(cin, jurusan[jumlahMahasiswa]);
+		cout << "Tahun Masuk: ";
+		cin >> tahunMasuk[jumlahMahasiswa];
+		cin.ignore();
+		jumlahMahasiswa++;
+		cout << "Mahasiswa berhasil ditambahkan!" << endl;
+	}
+	else {
+		cout << "Kapasitas maksimum mahasiswa telah tercapai." << endl;
+	}
+);
+//isi disini
+void tampilkanSemuaMahasiswa();
+//isi disini
+void algorithmacariMahasiswaByNIM();
+//isi disini
+void algorithmaSortByTahunMasuk();
+int main() {
+	int pilihan;
+	do {
+		cout << "========== MENU MANAJEMEN DATA MAHASISWA ==========" << endl;
+		cout << "1. Tambah Mahasiswa" << endl;
+		cout << "2. Tampilkan Semua Mahasiswa" << endl;
+		cout << "3. Cari Mahasiswa berdasarkan NIM" << endl;
+		cout << "4. Tampilkan Mahasiswa berdasarkan Tahun Masuk" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Pilihan: ";
+		cin >> pilihan;
+		cin.ignore();
+		switch (pilihan) {
+		case 1:
+			//isi disini
+			break;
+		case 2:
+			//isi disini
+			break;
+		case 3:
+			//isi disini
+			break;
+		case 4:
+			//isi disini
+			break;
+		case 5:
+			cout << "Terima kasih! Program selesai." << endl;
+			break;
+		default:
+			cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+		}
+		cout << endl;
+	} while (pilihan != 5);
+	return 0;
+}
+
+// 2. binary tree,linear search
+// 3. stack cara menyimpan data yang last in first out, queu data linear dimana elemen elemennya disisipkan di ujung belakang dan di hapus dari depan
+// 4. stack di gunakan saat kita ingin menampilkan data yang terakhir sebagai data yang pertama keluar.
+// 5 a. 4
+// 5 b. in =salah
+// 5. preorder = benar
+//     post benar
+
